65167a7b3a5ce33380a0c35e034c447dd6ebd8b7
Storm B. Heg <storm@stormbase.digital>
2024-12-01 14:12:55 +0100

    Add sandbox view for verifying with passkey only
    Having both an autofill username field AND a button causes issues for
    browser automation, as the browser will automatically use the autofill.

--- sandbox/urls.py
+++ sandbox/urls.py
@@ -2,10 +2,11 @@ from django.contrib.auth.urls import urlpatterns as auth_urls
 from django.urls import include, path
 
 from .admin import admin_site
-from .views import IndexView, SecondFactorVerificationView
+from .views import IndexView, LoginWithPasskeyView, SecondFactorVerificationView
 
 urlpatterns = [
     path("", IndexView.as_view(), name="index"),
+    path("login-passkey/", LoginWithPasskeyView.as_view(), name="login-passkey"),
     path(
         "verification/",
         SecondFactorVerificationView.as_view(),
