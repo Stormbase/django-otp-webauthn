d68c2ab548d86f12677810d786cb541353efe76d
Storm B. Heg <storm@stormbase.digital>
2025-10-24 16:59:44 +0200

    Add help to `update_translations.sh`

--- scripts/update_translations.sh
+++ scripts/update_translations.sh
@@ -9,6 +9,12 @@
 # Create or update existing language:
 # ./update_translations.sh -l <language_code>
 
+# If no arguments are provided, show usage
+if [ "$#" -eq 0 ]; then
+    echo "Usage: $0 [--all | -l <language_code>]"
+    exit 1
+fi
+
 set -e
 
 IGNORE_PATHS="--ignore venv --ignore client --ignore .tox --ignore dist --ignore node_modules --ignore docs"
